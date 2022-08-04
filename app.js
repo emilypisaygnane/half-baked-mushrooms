be30361dceb19d1b9f0a873800e117fe2a307ee3
Emily <episaygnane@me.com>
2022-08-04 09:19:42 -0700

    added friend button event listener

--- app.js
+++ app.js
@@ -39,10 +39,17 @@ addMushroomButton.addEventListener('click', () => {
 });
 
 addFriendButton.addEventListener('click', () => {
-
+    const name = friendInputEl.value;
+    const newFriend = {
+        name: name || `Friend #${Math.floor(Math.random() * 0)}`,
+        satisfaction: 1
     };
 
-    
+    friendData.push(newFriend);
+
+    friendInputEl.value = '';
+
+    displayFriends();
 });
 
 function findFriendByName(name, friends) {
